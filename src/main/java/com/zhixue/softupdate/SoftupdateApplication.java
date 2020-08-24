2a9a6cf4edea92e3ba90c367c26175fe9303ff25
sen <sen@136.com>
2020-08-24 17:26:36 +0800

    修改

--- src/main/java/com/zhixue/softupdate/SoftupdateApplication.java
+++ src/main/java/com/zhixue/softupdate/SoftupdateApplication.java
@@ -2,12 +2,17 @@ package com.zhixue.softupdate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SoftupdateApplication {
+public class SoftupdateApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SoftupdateApplication.class, args);
     }
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 }
